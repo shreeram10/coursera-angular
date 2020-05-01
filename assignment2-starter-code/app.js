658dcbf6ab0216f3753dbbdd240719c58b5461a7
Shreeram Patil <shreeramspatil@gmail.com>
2020-05-01 17:05:30 +0530

    Assignment_2

--- assignment2-starter-code/app.js
+++ assignment2-starter-code/app.js
@@ -1,29 +1,59 @@
-(function () {
-'use strict';
+(function(){
+  'use strict';
 
-angular.module('LunchCheckApp', [])
-.controller('LunchCheckController', LunchCheckController);
-function LunchCheckController($scope) {
-  $scope.lunchItems = "";
-  $scope.message = "";
-  $scope.len = 0;
-  
-  $scope.check = function(){
-    $scope.items = $scope.lunchItems.split(',');
-		$scope.lunchItems = $scope.items;
-    $scope.len =$scope.lunchItems.length;
+  angular.module('ShoppingListCheckOff',[])
+  .controller('ToBuyController',ToBuyController)
+  .controller('AlreadyBoughtController',AlreadyBoughtController)
+  .service('ShoppingListCheckOffService',ShoppingListCheckOffService);
 
-    if($scope.len==1 && $scope.lunchItems==""){
-      $scope.message = "Please enter data first";
-    } 
-    else if ($scope.len>0 && $scope.len<=3){
-      $scope.message = "Enjoy!";
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService) {
+    var showlist = this;
+    showlist.to_buy = ShoppingListCheckOffService.getItems();
+    showlist.removeItem = function (itemIndex) {
+      try{
+      ShoppingListCheckOffService.removeItem(itemIndex);
+    }catch (error){
+      showlist.errorMessage = error.message;
     }
-    else{
-      $scope.message = "Too Much!";
-    }
-	}
+
+      };
+  }
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService','$scope'];
+function AlreadyBoughtController(ShoppingListCheckOffService,$scope) {
+  var showlist = this;
+  $scope.IsVisible = false;
+  showlist.bought = ShoppingListCheckOffService.getItems1();
 }
 
 
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    var to_buy = [{ name: "cookies", quantity: 10 },{ name: "chips", quantity: 2 },{ name: "cold-drinks", quantity: 5 },{ name: "milk", quantity: 4 },{ name: "banana", quantity: 12 }];
+    var bought= [];
+
+    service.getItems = function () {
+      return to_buy;
+    };
+
+    service.getItems1 = function () {
+
+      return bought;
+    };
+
+    service.removeItem = function (itemIndex) {
+      if (to_buy.length !=1){
+        bought.push(to_buy[itemIndex]);
+        to_buy.splice(itemIndex,1);
+      }
+      else{
+        bought.push(to_buy[itemIndex]);
+        to_buy.splice(itemIndex,1);
+        throw new Error("Everything is bought!");
+      }
+    };
+
+  }
 })();
